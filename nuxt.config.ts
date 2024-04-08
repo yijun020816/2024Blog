dab63c70808d71a36c4f29a1734b7c5454757c56
凹凸曼 <2841159677@qq.com>
2024-04-08 13:39:44 +0800

    博客框架

---
+++ nuxt.config.ts
@@ -0,0 +1,78 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { siteConfig } from './site.config'
+
+export default defineNuxtConfig({
+  modules: [
+    '@unocss/nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/content',
+    '@nuxtjs/stylelint-module',
+  ],
+  devServer: {
+    host: '0.0.0.0',
+  },
+  app: {
+    rootId: 'nuxt-root',
+    head: {
+      meta: [
+        { name: 'description', content: siteConfig.description },
+        { name: 'author', content: siteConfig.author },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        { name: 'revisit-after', content: '7 days' },
+        { name: 'msapplication-TileColor', content: '#ffffff' },
+        { charset: 'UTF-8' },
+        { 'http-equiv': 'X-UA-Compatible', 'content': 'IE=edge' },
+      ],
+      noscript: [
+        { children: 'JavaScript is required' },
+      ],
+      link: [
+        {
+          type: 'text/css',
+          rel: 'stylesheet',
+          href: 'https://at.alicdn.com/t/c/font_3855138_irdxpj6pg9c.css',
+        },
+      ],
+      htmlAttrs: {
+        lang: siteConfig.lang,
+      },
+      bodyAttrs: {
+        class: 'font-sans',
+      },
+    },
+  },
+
+  content: {
+    highlight: {
+      theme: {
+        // Default theme (same as single string)
+        default: 'aurora-x',
+        // Theme used if `html.dark`
+        dark: 'github-dark',
+        // Theme used if `html.sepia`
+        sepia: 'monokai',
+      },
+      preload: [
+        'javascript',
+        'typescript',
+        'vue',
+        'vue-html',
+        'c',
+        'cpp',
+        'java',
+      ],
+
+    },
+  },
+  css: [
+    '@unocss/reset/tailwind.css',
+    '@/assets/styles/global.scss',
+    '@/assets/styles/theme.css',
+    '@/assets/styles/transition.css',
+    '@/assets/styles/markdown.scss',
+  ],
+  stylelint: {
+    /* module options */
+    lintOnStart: false,
+  },
+})
---
+++ pages/blog.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { getIncludedYearPosts } from '@/utils'
+
+const posts = await getIncludedYearPosts('blog')
+</script>
+
+<template>
+  <sub-nav />
+  <post-list :posts="posts" />
+</template>
