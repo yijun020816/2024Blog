9a8af94b54150f1da2fb9255d55c6604330f3a70
凹凸曼 <2841159677@qq.com>
2024-04-15 14:41:44 +0800

    更新项目页样式

--- pages/projects.vue
+++ pages/projects.vue
@@ -14,15 +14,15 @@ import { projectList } from '@/site.config'
       <div class="grid grid-cols-1 md:grid-cols-2 gap-1em">
         <div
           v-for="(project) in series.content" :key="project.path"
-          class="flex items-center py-0.5em px-1em rounded-sm hover:bg-gray-400:10"
+          class="flex items-center py-0.5em px-1em rounded-sm hover:bg-gray-400:10 cursor-auto"
         >
           <div class="hover w-full">
-            <a class="text-lg" target="_blank" :href="project.path">
+            <a class="text-lg hover:(border-dotted border-b-2 border-black)" :title="`打开${project.name}`" target="_blank" :href="project.path">
               {{ project.name }} <span class="text-sm op-50 i-icon-park-outline-efferent-three" />
             </a>
             <br>
-            <a class="text-sm op-70" target="_blank" :href="project.githubPath">
-              {{ project.desc }}<span class="i-icon-park-outline-github w-1em h-1em ml-2" title="github地址" />
+            <a class="text-sm op-70 hover:(border-dotted border-b-2 border-gray-400)" title="打开github地址" target="_blank" :href="project.githubPath">
+              {{ project.desc }}<span class="i-icon-park-outline-github w-1em h-1em ml-2" />
             </a>
           </div>
         </div>
